75e3b3b6043680eb23178064b9ac241db95b05ba
shinichi-kogai <kogai0121@gmail.com>
2015-07-09 10:16:42 +0900

    userモデル作成時にmailの重複が無いかチェック

--- Backend/classes/User.js
+++ Backend/classes/User.js
@@ -3,6 +3,7 @@
 const util = require('util')
 const uuid = require('uuid')
 const bcrypt = require('bcrypt');
+const Q = require('q')
 
 const log = require('Utils/common/log')
 const UserModel = require('Backend/models/User')
@@ -15,31 +16,43 @@ class User{
 
   create(userData, done){
     var _self = this
-    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt){
-      if(err){
-        return done(err)
+
+    this.Model
+    .query()
+    .where('mail', userData.mail)
+    .then(function(users){
+      if(users){
+        return done('登録済みのメールアドレスです。')
       }
-      bcrypt.hash(userData.password, salt, function(err, hash){
+      bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt){
         if(err){
           return done(err)
         }
+        bcrypt.hash(userData.password, salt, function(err, hash){
+          if(err){
+            return done(err)
+          }
 
-    		_self.Model
-    		.query()
-    		.insert({
-          userid: uuid.v1(),
-          isVerified: false,
-          name: userData.name,
-          mail: userData.mail,
-          password: hash
+      		_self.Model
+      		.query()
+      		.insert({
+            userid: uuid.v1(),
+            isVerified: false,
+            name: userData.name,
+            mail: userData.mail,
+            password: hash
+          })
+      		.then(function(user){
+            done(null, user)
+      		})
+      		.catch(function(err){
+      			return done(err)
+      		})
         })
-    		.then(function(user){
-          done(null, user)
-    		})
-    		.catch(function(err){
-    			return done(err)
-    		})
       })
+    })
+    .fail(function(err){
+      done(err)
     })
   }
 
@@ -69,47 +82,3 @@ class User{
 module.exports = function(){
   return new User()
 }
-
-/*
-UserSchema.pre('save', function(next) {
-	var _user = this;
-
-	// only hash the password if it has been modified (or is new)
-	if (!_user.isModified('password')){
-    return next();
-  }
-
-	var generateSalt = function() {
-		var d = Q.defer();
-		bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
-			if(err){
-        return next(err);
-      }
-
-			d.resolve(salt);
-		});
-		return d.promise;
-	};
-
-	var hashPassword = function(salt) {
-		var d = Q.defer();
-		bcrypt.hash(_user.password, salt, function(err, hash) {
-			if (err){
-        return next(err);
-      }
-
-			_user.password = hash;
-			d.resolve(hash);
-		});
-		return d.promise;
-	};
-
-	generateSalt()
-	.then(hashPassword)
-	.done(function(hash) {
-		next();
-	});
-});
-
-module.exports = db.model('user', UserSchema);
-*/
